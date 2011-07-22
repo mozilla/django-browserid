2d68bf520e9fea69f3a24b1ba739924d7ba52eb0
Paul Osman <paul@eval.ca>
2011-07-22 13:45:11 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+import django_browserid
+
+
 setup(
     name='django-browserid',
-    version='0.1dev',
+    version=django_browserid.__version__,
     packages=['django_browserid'],
     author="Paul Osman",
     author_email="paul@mozillafoundation.org",
