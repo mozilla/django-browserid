376504e2efe90abcb31cf029949e1679a545a9e1
groovecoder <luke.crouch@gmail.com>
2011-12-29 12:20:55 -0600

    authenticated users need browserid to change their email

--- django_browserid/context_processors.py
+++ django_browserid/context_processors.py
@@ -4,9 +4,5 @@ from django_browserid.forms import BrowserIDForm
 def browserid_form(request):
     """
     A context processor that adds a BrowserID form to the request
-    when the user is not authenticated
     """
-    if request.user.is_authenticated():
-        return {}
-    else:
-        return {'browserid_form': BrowserIDForm()}
+    return {'browserid_form': BrowserIDForm()}
