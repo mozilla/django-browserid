5635cdbc4e493d27dd6ab77ae92cf70ae78e2deb
Thomas Grainger <tagrain@gmail.com>
2012-02-28 22:47:06 +0000

    Updated to use the django form media standard

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     packages=['django_browserid'],
     author="Paul Osman",
     author_email="paul@mozillafoundation.org",
-    install_requires="requests==0.9.1"
+    install_requires="requests==0.9.1",
+    include_package_data=True,
+    zip_safe = False,
 )
