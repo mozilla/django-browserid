c10123e54a899ef2746cb8d611ecdf81a865f3d7
Michael Kelly <mkelly@mozilla.com>
2012-03-29 14:59:22 -0700

    Update setup.py and add symlink to README.rst.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='django-browserid',
     version='0.2',
     packages=['django_browserid'],
-    author="Paul Osman",
-    author_email="paul@mozillafoundation.org",
-    install_requires="requests==0.9.1",
-    package_data={"django_browserid": ["static/browserid/*.js"]},
+    author='Paul Osman, Michael Kelly',
+    author_email='mkelly@mozilla.com',
+    url='https://github.com/mozilla/django-browserid',
+    install_requires='requests>=0.9.1',
+    package_data={'django_browserid': ['static/browserid/*.js']},
 )
