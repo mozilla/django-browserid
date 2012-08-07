1d2d9459cd3d37087a2d792d5f2a40a6437fb486
jrconlin <jrconlin+git@mozilla.com>
2012-08-07 16:03:02 -0700

    Bug 780223 - Fixes for Persona changes
    Change to use watch() instead of getVerifiedEmail()

--- django_browserid/static/browserid/browserid.js
+++ django_browserid/static/browserid/browserid.js
@@ -1,14 +1,37 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* The persona documentation is at
+ * https://developer.mozilla.org/en-US/docs/BrowserID/Quick_Setup
+ * and requests that login and logout callbacks are included so
+ * that accounts can be managed.
+ */
  $(document).ready(function() {
     $('#browserid').bind('click', function(e) {
         e.preventDefault();
-        navigator.id.getVerifiedEmail(function(assertion) {
+        navigator.id.request();
+    };
+    $('.logout').bind('click', function(e) {
+        /* obviously, this should also be called for any other
+         * logout like action. Standard action can be applied
+         * before and after this call.
+         */
+        navigator.id.logout();
+    };
+    navigator.id.watch(
+        onlogin: function(assertion) {
             if (assertion) {
                 var $e = $('#id_assertion');
                 $e.val(assertion.toString());
                 $e.parent().submit();
+            }
+        },
+        onlogout: function() {
+            /* no additional action required, however
+             * callback must be provided
+             */
+            if (typeof $e.parent().logout === 'function') {
+                $e.parent().logout()
             }
         });
     });
