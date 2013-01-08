46db8c6a61105b8c4e02bb79e2019502d00d6995
prasoon2211 <prasoon92.iitr@gmail.com>
2013-01-08 12:46:32 +0530

    Fixed issue 110. Added an extra kwarg to BrowserIDBackend.authenticate that is now being passed to verify.

--- django_browserid/auth.py
+++ django_browserid/auth.py
@@ -62,7 +62,7 @@ class BrowserIDBackend(object):
 
         return self.User.objects.create_user(username, email)
 
-    def authenticate(self, assertion=None, audience=None, **kw):
+    def authenticate(self, assertion=None, audience=None, browserid_extra=None, **kw):
         """``django.contrib.auth`` compatible authentication method.
 
         Given a BrowserID assertion and an audience, it attempts to
@@ -74,7 +74,7 @@ class BrowserIDBackend(object):
 
         See django_browserid.base.get_audience()
         """
-        result = verify(assertion, audience, **kw)
+        result = verify(assertion, audience, extra_params=browserid_extra)
         if not result:
             return None
 
