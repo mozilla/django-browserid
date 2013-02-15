c58f8d1c3db861e36191d4bcd5e4385a371f189a
Peter Bengtsson <mail@peterbe.com>
2013-02-15 13:57:19 -0800

    version number in one place

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import codecs
+import os
+import re
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as file:
-    long_description = file.read()
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='django-browserid',
     description='Django application for adding BrowserID support.',
-    long_description=long_description,
-    version='0.7.1',
+    long_description=read('README.rst'),
+    version=find_version('django_browserid/__init__.py'),
     packages=find_packages(),
     author='Paul Osman, Michael Kelly',
     author_email='mkelly@mozilla.com',
