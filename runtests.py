681a898d9dba89c70fe7f07bc61d1b84258f092b
Thomas Grainger <tagrain@gmail.com>
2013-04-05 15:45:09 +0100

    ensure settings are imported before django_browserid

--- runtests.py
+++ runtests.py
@@ -1,12 +1,13 @@
-#This file mainly exists to allow python setup.py test to work.
+# This file mainly exists to allow python setup.py test to work.
 # http://ericholscher.com/blog/2009/jun/29/enable-setuppy-test-your-django-apps/
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'django_browserid.tests.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 os.environ['REUSE_DB'] = '0'
 
-print sys.path
+test_dir = os.path.join(os.path.dirname(__file__),'django_browserid/tests')
+sys.path.insert(0, test_dir)
 
 from django.test.utils import get_runner
 from django.conf import settings
