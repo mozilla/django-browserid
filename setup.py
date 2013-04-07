990963145b037f72c5c39a033b64382c8e6bea6e
Thomas Grainger <tagrain@gmail.com>
2013-04-07 19:28:41 +0100

    setup.py: in version checking code use utf8 encoding

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 def read(*parts):
-    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts), encoding="utf8").read()
 
 
 def find_version(*file_paths):
