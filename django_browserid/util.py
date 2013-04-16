fd45b0f568981eaf9d1c3fe2ec71b6c7b143077c
Michael Kelly <mkelly@mozilla.com>
2013-04-16 10:09:54 -0400

    Add support for lazily-evaluated BROWSERID_REQUEST_ARGS.
    Updates our use of BROWSERID_REQUEST_ARGS to support Django Promises.
    This is primarily useful for translating strings in the request args
    using lazy translation, as well as reversing URLs using reverse_lazy.

--- django_browserid/util.py
+++ django_browserid/util.py
@@ -1,6 +1,22 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import json
+
+from django.utils.functional import Promise
+from django.utils.encoding import force_unicode
+
+
+class LazyEncoder(json.JSONEncoder):
+    """
+    JSONEncoder that turns Promises into unicode strings to support functions
+    like ugettext_lazy and reverse_lazy.
+    """
+    def default(self, obj):
+        if isinstance(obj, Promise):
+            return force_unicode(obj)
+        return super(LazyEncoder, self).default(obj)
+
 
 # Attempt to use staticfiles_storage.url to retrieve static file URLs.
 # If it can't be found (Django 1.3), default to prepending settings.STATIC_URL
@@ -14,3 +30,4 @@ except ImportError:
     def static_url(path):
         """Return a public URL for the given static file path."""
         return ''.join([settings.STATIC_URL, path])
+
